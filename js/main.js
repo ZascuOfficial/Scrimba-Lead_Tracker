e0e828f3959c8d83c02dd86b816f3128819aadb8
Zascu <zascu.github@gmail.com>
2022-11-12 01:21:10 +0200

    Finish the functionality of the app

--- js/main.js
+++ js/main.js
@@ -1,26 +1,28 @@
 let myLeads = [];
 
 const inputEl = document.getElementById('input-el');
-const inputBtn = document.getElementById('input-btn');
 const ulEl = document.getElementById('ul-el');
 
-inputBtn.addEventListener('click', function () {
-  
-  myLeads.push(inputEl.value);
-  inputEl.value = '';
-  
-  renderLeads();
+const inputBtn = document.getElementById('input-btn');
+const tabBtn = document.getElementById('tab-btn');
+const deleteBtn = document.getElementById('delete-btn');
 
-})
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'));
 
-function renderLeads() {
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+
+  render(myLeads);
+}
+
+function render(leads) {
   let listItems = '';
 
-  for (let i = 0; i < myLeads.length; i++) {
+  for (let i = 0; i < leads.length; i++) {
     listItems += `
       <li>
-        <a target='_blank' href='${myLeads[i]}'>
-          ${myLeads[i]}
+        <a target='_blank' href='https://${leads[i]}'>
+          https://${leads[i]}
         </a>
       </li>
     `;
@@ -28,3 +30,40 @@ function renderLeads() {
 
   ulEl.innerHTML = listItems;
 }
+
+inputBtn.addEventListener('click', function () {
+
+  myLeads.push(inputEl.value);
+  inputEl.value = '';
+  localStorage.setItem('myLeads', JSON.stringify(myLeads));
+
+  render(myLeads);
+
+});
+
+tabBtn.addEventListener('click', function () {
+
+  chrome.tabs.query(
+    {
+      active: true,
+      currentWindow: true
+    },
+
+    function (tabs) {
+      myLeads.push(tabs[0].url);
+      localStorage.setItem('myLeads', JSON.stringify(myLeads));
+
+      render(myLeads);
+    }
+  );
+
+});
+
+deleteBtn.addEventListener('dblclick', function () {
+
+  localStorage.clear();
+  myLeads = [];
+
+  render(myLeads);
+
+});
